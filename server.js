b71e31dd182848c048fdef8c230dcaf1ce5be442
Tarun <ytarun361@gmail.com>
2026-07-02 19:54:37 +0530

    trying to fix choppyness

--- server.js
+++ server.js
@@ -90,4 +90,5 @@ function checkWinCondition() {
     }
 }
 
-http.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));

e00c2e0dfd01c2bf6b6755c8481490cabb468a15
Tarun <ytarun361@gmail.com>
2026-07-07 17:27:30 +0530

    path prob

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public'))); 
 let players = {};
 let gameStarted = false;
 
